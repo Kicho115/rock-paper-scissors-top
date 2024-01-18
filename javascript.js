bad4c4ffe1eeb6e2350c1d6c7c21557ab19ce7ac
Kicho115 <oscarleonardo426@gmail.com>
2024-01-17 21:48:04 -0800

    Add posible choices and computer and player choices

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let possibleChoices = ['rock', 'paper', 'scissors'];
+let randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
+
+let computerChoice = randomChoice;
+let playerChoice = prompt('Rock, Paper or Scissors?', randomChoice);
+
+function playRound(playerChoise, computerChoice) {
+
+}
