8383efb8171c3b213fe99bfa31eec4e0f717c7da
Kicho115 <oscarleonardo426@gmail.com>
2024-01-17 22:16:19 -0800

    Add a function to play a round

--- javascript.js
+++ javascript.js
@@ -4,8 +4,45 @@ let randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.le
 let computerChoice = randomChoice;
 let playerChoice = prompt('Rock, Paper or Scissors?', randomChoice);
 
-let winMessage =   `You Win! ${computerChoice} beats ${playerChoice}`;
-let defeatMessage = `You Lose! ${computerChoice} beats ${playerChoice}`;
-let tieMessage = "It's a Tie!";
+function playRound(playerChoice, computerChoice) {
+    // Make the player choice case-insensitive
+    playerChoice = playerChoice.toLowerCase();
 
+    let winMessage =   `You Win! ${computerChoice} beats ${playerChoice}`;
+    let defeatMessage = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    let tieMessage = "It's a Tie!";
 
+    switch(playerChoice) {
+        case 'rock':
+            if (computerChoice == 'scissors') {
+                console.log(winMessage)
+            } else if (computerChoice == 'paper') {
+                console.log(defeatMessage)
+            } else {
+                console.log(tieMessage)
+            }
+            break;
+        case 'paper':
+                if (computerChoice == 'rock') {
+                    console.log(winMessage)
+                } else if (computerChoice == 'scissors') {
+                    console.log(defeatMessage)
+                } else {
+                    console.log(tieMessage)
+                }
+                break;
+        case 'scissors':
+            if (computerChoice == 'paper') {
+                console.log(winMessage)
+            } else if (computerChoice == 'rock') {
+                console.log(defeatMessage)
+            } else {
+                console.log(tieMessage)
+            }
+            break;
+        
+    }
+
+}
+
+playRound(playerChoice, computerChoice)
