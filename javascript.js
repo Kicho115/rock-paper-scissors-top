d0c459f50cc33ef40f72737624658fccae4fcadb
Kicho115 <oscarleonardo426@gmail.com>
2024-03-12 11:17:02 -0700

    Add visuals to represent choices

--- javascript.js
+++ javascript.js
@@ -1,15 +1,32 @@
 let possibleChoices = ['rock', 'paper', 'scissors'];
+let possibleChoicesIcons = {
+    rock: '🪨',
+    paper: '📄',
+    scissors: '✂️'
+};
+
 let randomChoice = () => possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 
 let playerScore = 0;
 let computerScore = 0;
 
+let playerScoreDisplay = document.getElementById('playerScore');
+let computerScoreDisplay = document.getElementById('computerScore');
+let playerChoiceIcon = document.getElementById('playerChoice');
+let computerChoiceIcon = document.getElementById('computerChoice');
+let message = document.getElementById('message');
+
 let selections = document.getElementById('selections');
 
+// Game
 selections.addEventListener('click', (e) => {
     let computerChoice = randomChoice();
     let playerChoice = e.target.id;
+    playerChoiceIcon.innerText = possibleChoicesIcons[playerChoice];
+    computerChoiceIcon.innerText = possibleChoicesIcons[computerChoice];
     playRound(playerChoice, computerChoice);
+    playerScoreDisplay.innerText = playerScore;
+    computerScoreDisplay.innerText = computerScore;
 });
 
 function playRound(playerChoice, computerChoice) {
@@ -23,6 +40,7 @@ function playRound(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
         console.log(tieMessage);
+        message.innerText = tieMessage;
         return;
     } else if (!possibleChoices.includes(playerChoice)) {
         console.log(invalidInputMessage);
@@ -32,31 +50,54 @@ function playRound(playerChoice, computerChoice) {
         case 'rock':
             if (computerChoice === 'scissors') {
                 console.log(winMessage);
+                message.innerText = winMessage;
                 playerScore++;
             } else if (computerChoice === 'paper') {
                 console.log(defeatMessage);
+                message.innerText = defeatMessage;
                 computerScore++;
             }
             break;
         case 'paper':
                 if (computerChoice === 'rock') {
                     console.log(winMessage);
+                    message.innerText = winMessage;
                     playerScore++;
                 } else if (computerChoice === 'scissors') {
                     console.log(defeatMessage);
+                    message.innerText = defeatMessage;
                     computerScore++;
                 }
                 break;
         case 'scissors':
             if (computerChoice === 'paper') {
                 console.log(winMessage);
+                message.innerText = winMessage;
                 playerScore++;
             } else if (computerChoice === 'rock') {
                 console.log(defeatMessage);
+                message.innerText = defeatMessage;
                 computerScore++;
             }
             break;
         
     }
 
+    scoreCheck();
+}
+
+// Check if the score of either player is 5
+function scoreCheck() {
+    if (playerScore === 5) {
+        // player wins
+        console.log('ganas');
+    }
+    else if (computerScore === 5) {
+        //computer wins
+        console.log('pierdes');
+    }
+}
+
+function restartGame() {
+    
 }
